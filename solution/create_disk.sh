af23ad8a0d1cffadcdce4823d00afa1a2f7d9ef1
LukeBlattner <lukeblattner@gmail.com>
2024-11-26 20:05:18 -0600

    Added changes to create disk

--- solution/create_disk.sh
+++ solution/create_disk.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-dd if=/dev/zero of=disk.img bs=1M count=1
+#Need to make a variable for name of disk.img, size, and count?
+
+#No idea if this is right
+
+diskName = $1
+diskSize = $2
+diskCount = $3
+
+#default is disk.img 1M 1
+dd if=/dev/zero of=$diskName bs=$diskSize count=$diskCount

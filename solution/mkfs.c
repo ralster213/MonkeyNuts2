80e1fb0bd1021a6a32c648df430d6ef1a729a658
LukeBlattner <lukeblattner@gmail.com>
2024-11-26 19:22:46 -0600

    Done taking in args?

--- solution/mkfs.c
+++ solution/mkfs.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[]) {
     int *arr = malloc(sizeof(int) * argc);
     int counter = 0;
     int diskOne = 0; //make sure we have at least one disk
+    int lastDisk = 0;
 
     //Goes through and finds the locations of the disks
     while(counter < argc) {
@@ -32,11 +33,38 @@ int main(int argc, char *argv[]) {
             diskOne = 1; //we have at least one disk
             counter++;
             arr[counter] = 1;
+            lastDisk = counter;
             counter++;
         }
         else {
-            moreDisks = 0;
+            counter += 1;
         }
     }
+
+    counter++;
+
+    if ((strcmp(argv[counter], "-i")) != 0) {
+        return -1;
+    }
+
+    //Number of inodes can be anything
+    counter++;
+    inode = argv[counter];
+
+
+    counter++;
+    if ((strcmp(argv[counter], "-b")) != 0) {
+        return -1;
+    }
+
+    counter++;
+    int numberOfBlocks = argv[counter];
+
+    if (numberOfBlocks % 32 != 0) {
+        numberOfBlocks += (32 - (numberOfBlocks % 32));
+    }
+
+
+
 
 }

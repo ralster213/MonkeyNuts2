9b3b3cb2ab5121021680357e47aa5d74276c3768
ralster213 <raleigh.page2@gmail.com>
2024-11-26 20:13:50 -0600

    Updated create_disk.sh

--- solution/create_disk.sh
+++ solution/create_disk.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#Need to make a variable for name of disk.img, size, and count?
-
-#No idea if this is right
-
-diskName = $1
-diskSize = $2
-diskCount = $3
-
-#default is disk.img 1M 1
-dd if=/dev/zero of=$diskName bs=$diskSize count=$diskCount
+dd if=/dev/zero of=$1 bs=$2 count=$3

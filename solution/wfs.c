c079455766352cbf5bb146913b9d925cc1d27631
ralster213 <raleigh.page2@gmail.com>
2024-12-04 13:02:12 -0600

    Wrote skeleton code for callback functions

--- solution/wfs.c
+++ solution/wfs.c
@@ -5,19 +5,78 @@
 #define <errno.h>
 */
 
-
-#define etc 30
+#define FUSE_USE_VERSION 30
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 
-static int my_getattr(const char *path, struct stat *stbuf) {
+static int wfs_getattr(const char *path, struct stat *stbuf) {
     // Implementation of getattr function to retrieve file attributes
     // Fill stbuf structure with the attributes of the file/directory indicated by path
     // ...
 
     return 0; // Return 0 on success
+}
+
+static int wfs_unlink(const char* path) {
+    // Remove (delete) the given file, symbolic link, hard link, or special node
+    // Note that if you support hard links, unlink only deletes the data when the last hard link is removed. See unlink(2) for details.
+
+}
+
+static int wfs_mknod(const char* path, mode_t mode, dev_t rdev) {
+    // Make a special (device) file, FIFO, or socket. See mknod(2) for details. 
+
+}
+
+
+static int wfs_mkdir(const char* path, mode_t mode) {
+    // Create a directory with the given name. 
+    // The directory permissions are encoded in mode. 
+    // See mkdir(2) for details. This function is needed for any reasonable read/write filesystem.
+    return 0; // Success
+
+}
+
+static int wfs_rmdir(const char* path) {
+    // Remove the given directory. This should succeed only if the directory is empty (except for "." and ".."). See rmdir(2) for details.
+    // You should free all directory data blocks
+    // but you do NOT need to free directory data blocks when unlinking files in a directory
+    return 0; // Return 0 on success
+}
+
+static int wfs_read(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+    // Read sizebytes from the given file into the buffer buf, beginning offset bytes into the file. See read(2) for full details. 
+    // Returns the number of bytes transferred, or 0 if offset was at or beyond the end of the file.
+    if (/*offset was at or beyond the end of the file*/1) {
+        return 0;
+    }
+    size_t bytes_transfered = 0;
+    return bytes_transfered;
+}
+
+static int wfs_write(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+    // As for read above, except that it can't return 0.
+
+    size_t bytes_transfered = 0;
+    return bytes_transfered;
+}
+
+static int wfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
+    /*
+    *Return one or more directory entries (struct dirent) to the caller. 
+    *This is one of the most complex FUSE functions. 
+    *It is related to, but not identical to, the readdir(2) and getdents(2) system calls, and the readdir(3) library function. 
+    *Because of its complexity, it is described separately below. 
+    *Required for essentially any filesystem, since it's what makes ls and a whole bunch of other things work.
+    */
+
+   // return -EBADF if the file handle is invalid, or -ENOENT if you use the path argument and the path doesn't exist
+   return 0;
+   // struct dirent directory;
+   //return directory;
+
 }
 
 static struct fuse_operations ops = {

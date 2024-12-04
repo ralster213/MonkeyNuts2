cde6854bd87f0483a3130ee50296cd47773ec1da
Luke <lukeblattner@gmail.com>
2024-12-03 21:29:06 -0600

    squiggles

--- solution/wfs.c
+++ solution/wfs.c
@@ -1,9 +1,52 @@
+/*
 #define etc 30
 #include <fuse.h>
 #define FUSE_USE_VERSION 30
 #define <errno.h>
+*/
 
 
+#define etc 30
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+static int my_getattr(const char *path, struct stat *stbuf) {
+    // Implementation of getattr function to retrieve file attributes
+    // Fill stbuf structure with the attributes of the file/directory indicated by path
+    // ...
+
+    return 0; // Return 0 on success
+}
+
+static struct fuse_operations ops = {
+  .getattr = wfs_getattr,
+  .mknod   = wfs_mknod,
+  .mkdir   = wfs_mkdir,
+  .unlink  = wfs_unlink,
+  .rmdir   = wfs_rmdir,
+  .read    = wfs_read,
+  .write   = wfs_write,
+  .readdir = wfs_readdir,
+};
+
+/*
+int main(int argc, char *argv[]) {
+    // Initialize FUSE with specified operations
+    // Filter argc and argv here and then pass it to fuse_main
+    return fuse_main(argc, argv, &ops, NULL);
+}
+
+
+*/
+
+
+//use super block to know disk information
+
+// put all operations in here
+
+//how do we find the disks? We could call ls to list disks
 
 int main(int argc, char *argv[]) {
 
